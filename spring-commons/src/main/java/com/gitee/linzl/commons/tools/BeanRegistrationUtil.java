4c8cb975d445b7c1dce62b410e6d69be59e85469
linzhenlie <linzhenlie-jk@360jinrong.net>
2020-10-27 15:36:51 +0800

    +refactor

--- spring-commons/src/main/java/com/gitee/linzl/commons/tools/BeanRegistrationUtil.java
+++ spring-commons/src/main/java/com/gitee/linzl/commons/tools/BeanRegistrationUtil.java
@@ -3,13 +3,13 @@ package com.gitee.linzl.commons.tools;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 
 import java.util.Map;
 import java.util.Objects;
 
 /**
  * copy from apollo client
+ * 注册一个bean信息
  *
  * @author linzhenlie
  * @date 2020-07-02
@@ -24,14 +24,24 @@ public class BeanRegistrationUtil {
         return registerBeanDefinitionIfNotExists(registry, beanName, beanClass, null);
     }
 
+    /**
+     * 当bean不存在时注册bean
+     *
+     * @param registry            spring容器
+     * @param beanName            注册bean的名称
+     * @param beanClass           需要注册的beanClass
+     * @param extraPropertyValues 添加bean的属性及属性值
+     * @return
+     */
     public static boolean registerBeanDefinitionIfNotExists(BeanDefinitionRegistry registry, String beanName,
                                                             Class<?> beanClass, Map<String, Object> extraPropertyValues) {
+        // 容器中是否已经存在该bean
         if (registry.containsBeanDefinition(beanName)) {
             return false;
         }
 
+        // 所有候选bean
         String[] candidates = registry.getBeanDefinitionNames();
-
         for (String candidate : candidates) {
             BeanDefinition beanDefinition = registry.getBeanDefinition(candidate);
             if (Objects.equals(beanDefinition.getBeanClassName(), beanClass.getName())) {
@@ -39,13 +49,14 @@ public class BeanRegistrationUtil {
             }
         }
 
-        BeanDefinition beanDefinition = BeanDefinitionBuilder.genericBeanDefinition(beanClass).getBeanDefinition();
+        BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(beanClass);
         if (Objects.nonNull(extraPropertyValues)) {
-            for (Map.Entry<String, Object> entry : extraPropertyValues.entrySet()) {
-                beanDefinition.getPropertyValues().add(entry.getKey(), entry.getValue());
-            }
+            // 设置普通类型属性值
+            extraPropertyValues.entrySet().stream().forEach(entry -> builder.addPropertyValue(entry.getKey(), entry.getValue()));
         }
-
+        //获取BeanDefinition
+        BeanDefinition beanDefinition = builder.getBeanDefinition();
+        //向bean注册器(容器)中注册bean
         registry.registerBeanDefinition(beanName, beanDefinition);
         return true;
     }
