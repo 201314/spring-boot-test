f91b5b8725262d98fd17516cb851292666dea05c
linzhenlie <linzhenlie-jk@360jinrong.net>
2021-03-10 15:25:40 +0800

    Slow http 拒绝服务

--- spring-commons/src/main/java/com/gitee/linzl/commons/config/CommonConfig.java
+++ spring-commons/src/main/java/com/gitee/linzl/commons/config/CommonConfig.java
@@ -1,12 +1,23 @@
 package com.gitee.linzl.commons.config;
 
-import com.alibaba.fastjson.serializer.*;
+import com.alibaba.fastjson.serializer.JSONSerializer;
+import com.alibaba.fastjson.serializer.ObjectSerializer;
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializeWriter;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.gitee.linzl.commons.constants.GlobalConstants;
 import com.gitee.linzl.commons.fileupload.CustomMultipartResolver;
 import com.gitee.linzl.commons.interceptor.DefaultPermissionTokenInterceptor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.coyote.AbstractProtocol;
+import org.apache.coyote.ProtocolHandler;
+import org.apache.coyote.UpgradeProtocol;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,8 +26,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.util.ClassUtils;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.config.annotation.*;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import java.io.IOException;
@@ -142,27 +157,6 @@ public class CommonConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
-    /**
-     * 描述 : 国际化拦截器
-     * <p>
-     * 如果是需要在界面上进行切换国际化，根据 url?local=国际化语言标识 自动国际化
-     * <p>
-     * 语言标识java.util.Locale
-     *
-     * @return
-     */
-    @Bean
-    public HandlerInterceptorAdapter localeChangeInterceptor() {
-        if (log.isDebugEnabled()) {
-            log.debug("初始化国际化拦截器,url?local=语言标识");
-        }
-        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-        // 根据locale变量动态改变语言
-        localeChangeInterceptor.setParamName(LocaleChangeInterceptor.DEFAULT_PARAM_NAME);
-        // 忽略无效的语言
-        localeChangeInterceptor.setIgnoreInvalidLocale(true);
-        return localeChangeInterceptor;
-    }
 
     /**
      * 描述 : 文件上传处理器
@@ -191,16 +185,47 @@ public class CommonConfig implements WebMvcConfigurer {
         registry.addConverter(new FormStringToDateConverter.StringToLocalDateTimeConverter());
     }
 
-    @Bean
-    public DefaultPermissionTokenInterceptor tokenInterceptor() {
-        return new DefaultPermissionTokenInterceptor();
-    }
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(this.tokenInterceptor()).addPathPatterns("/**")
+        /**
+         *  描述 : 国际化拦截器
+         *  如果是需要在界面上进行切换国际化，根据 url?local=国际化语言标识 自动国际化
+         *  语言标识java.util.Locale
+         */
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        // 根据locale变量动态改变语言
+        localeChangeInterceptor.setParamName(LocaleChangeInterceptor.DEFAULT_PARAM_NAME);
+        // 忽略无效的语言
+        localeChangeInterceptor.setIgnoreInvalidLocale(true);
+        registry.addInterceptor(localeChangeInterceptor);
+
+        registry.addInterceptor(new DefaultPermissionTokenInterceptor()).addPathPatterns("/**")
                 .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
-//		registry.addInterceptor(new ControllerValidatorInterceptor()).addPathPatterns("/**");
     }
 
+    /**
+     * Slow http 拒绝服务原理:
+     * 请求以很低的速度发送post请求数据包，当客户端连接了许多以后，占用了所有webserver可用连接，从而导致服务夯死。
+     * http慢速攻击是利用http合法机制，在建立连接后，尽量长时间保持连接，不释放，从而达到对HTTP服务攻击,
+     * 攻击者发送POST请求，自行构造报文向服务器提交数据，将报文长度设置一个很大的值，且在随后每次发送中，每次只发送一个很小的报文，这样导致服务器一直等待数据，连接始终一直被占用。
+     * 如果攻击者使用多线程或傀儡机子去做同样操作，服务器WEB容器很快就被占满TCP连接从而不再接受新请求。
+     */
+    @ConditionalOnClass({Tomcat.class, UpgradeProtocol.class})
+    @Bean
+    public TomcatConnectorCustomizer tomcatConnectorCustomizer() {
+        return new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                ProtocolHandler handler = (ProtocolHandler) connector.getProtocolHandler();
+                if (handler instanceof AbstractProtocol) {
+                    AbstractProtocol<?> protocol = (AbstractProtocol<?>) handler;
+                    //设置最大连接数
+                    protocol.setMaxConnections(2000);
+                    //设置最大线程数
+                    protocol.setMaxThreads(2000);
+                    protocol.setConnectionTimeout(30000);
+                }
+            }
+        };
+    }
 }
