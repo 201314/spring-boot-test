cf3893541699e2a2b2402e606d57f34893104def
linzl <2225010489@qq.com>
2021-06-18 11:41:38 +0800

    * 异常增加返回数据

--- spring-commons/src/main/java/com/gitee/linzl/commons/exception/BusinessException.java
+++ spring-commons/src/main/java/com/gitee/linzl/commons/exception/BusinessException.java
@@ -10,12 +10,19 @@ import com.gitee.linzl.commons.enums.IBaseErrorCode;
  * @date 2019/8/26
  */
 public class BusinessException extends ServiceException {
+    private Object data;
 
     public BusinessException(IBaseErrorCode errorEnum) {
         super(errorEnum);
     }
 
+    public BusinessException(IBaseErrorCode errorEnum, Object data) {
+        super(errorEnum);
+        this.data = data;
+    }
+
     public BusinessException(ApiResult api) {
         super(api);
+        this.data = data;
     }
 }
