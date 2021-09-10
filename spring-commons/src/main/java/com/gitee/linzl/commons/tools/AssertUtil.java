80128df3ffbcff82c35d895f6648e1321aa2a42f
linzl <2225010489@qq.com>
2021-09-10 18:05:20 +0800

    * AssertUtil增加日志打印

--- spring-commons/src/main/java/com/gitee/linzl/commons/tools/AssertUtil.java
+++ spring-commons/src/main/java/com/gitee/linzl/commons/tools/AssertUtil.java
@@ -21,137 +21,97 @@ import java.util.Objects;
  */
 @Slf4j
 public abstract class AssertUtil {
-    /**
-     * Assert a boolean expression, throwing an {@code IllegalStateException}
-     * if the expression evaluates to {@code false}.
-     * <p>Call {@link #isTrue} if you wish to throw an {@code IllegalArgumentException}
-     * on an assertion failure.
-     * <pre class="code">Assert.state(id == null, "The id property must not already be initialized");</pre>
-     *
-     * @param expression a boolean expression
-     * @param message    the exception message to use if the assertion fails
-     * @throws IllegalStateException if {@code expression} is {@code false}
-     */
-    public static void state(boolean expression, IBaseErrorCode message) {
-        if (!expression) {
-            throw new BusinessException(message);
-        }
-    }
-
-    /**
-     * Assert a boolean expression, throwing an {@code IllegalArgumentException}
-     * if the expression evaluates to {@code false}.
-     * <pre class="code">Assert.isTrue(i &gt; 0, "The value must be greater than zero");</pre>
-     *
-     * @param expression a boolean expression
-     * @param message    the exception message to use if the assertion fails
-     * @throws IllegalArgumentException if {@code expression} is {@code false}
-     */
     public static void isTrue(boolean expression, IBaseErrorCode message) {
         if (!expression) {
             throw new BusinessException(message);
         }
     }
 
-    /**
-     * Assert that an object is {@code null}.
-     * <pre class="code">Assert.isNull(value, "The value must be null");</pre>
-     *
-     * @param object  the object to check
-     * @param message the exception message to use if the assertion fails
-     * @throws IllegalArgumentException if the object is not {@code null}
-     */
+    public static void isTrue(boolean expression, IBaseErrorCode message, String printLog) {
+        if (!expression) {
+            log.error(printLog);
+            throw new BusinessException(message);
+        }
+    }
+
     public static void isNull(Object object, IBaseErrorCode message) {
         if (Objects.nonNull(object)) {
             throw new BusinessException(message);
         }
     }
 
-    /**
-     * Assert that an object is not {@code null}.
-     * <pre class="code">Assert.notNull(clazz, "The class must not be null");</pre>
-     *
-     * @param object  the object to check
-     * @param message the exception message to use if the assertion fails
-     * @throws IllegalArgumentException if the object is {@code null}
-     */
+    public static void isNull(Object object, IBaseErrorCode message, String printLog) {
+        if (Objects.nonNull(object)) {
+            log.error(printLog);
+            throw new BusinessException(message);
+        }
+    }
+
     public static void notNull(Object object, IBaseErrorCode message) {
         if (Objects.isNull(object)) {
             throw new BusinessException(message);
         }
     }
 
-    /**
-     * Assert that the given String is not empty; that is,
-     * it must not be {@code null} and not the empty String.
-     * <pre class="code">Assert.hasLength(name, "Name must not be empty");</pre>
-     *
-     * @param text    the String to check
-     * @param message the exception message to use if the assertion fails
-     * @throws IllegalArgumentException if the text is empty
-     * @see StringUtils#hasLength
-     */
+    public static void notNull(Object object, IBaseErrorCode message, String printLog) {
+        if (Objects.isNull(object)) {
+            log.error(printLog);
+            throw new BusinessException(message);
+        }
+    }
+
     public static void notEmpty(String text, IBaseErrorCode message) {
         if (!StringUtils.hasLength(text)) {
             throw new BusinessException(message);
         }
     }
 
-    /**
-     * Assert that an array contains elements; that is, it must not be
-     * {@code null} and must contain at least one element.
-     * <pre class="code">Assert.notEmpty(array, "The array must contain elements");</pre>
-     *
-     * @param array   the array to check
-     * @param message the exception message to use if the assertion fails
-     * @throws IllegalArgumentException if the object array is {@code null} or contains no elements
-     */
+    public static void notEmpty(String text, IBaseErrorCode message, String printLog) {
+        if (!StringUtils.hasLength(text)) {
+            log.error(printLog);
+            throw new BusinessException(message);
+        }
+    }
+
     public static void notEmpty(Object[] array, IBaseErrorCode message) {
         if (ObjectUtils.isEmpty(array)) {
             throw new BusinessException(message);
         }
     }
 
-    /**
-     * Assert that a collection contains elements; that is, it must not be
-     * {@code null} and must contain at least one element.
-     * <pre class="code">Assert.notEmpty(collection, "Collection must contain elements");</pre>
-     *
-     * @param collection the collection to check
-     * @param message    the exception message to use if the assertion fails
-     * @throws IllegalArgumentException if the collection is {@code null} or
-     *                                  contains no elements
-     */
+    public static void notEmpty(Object[] array, IBaseErrorCode message, String printLog) {
+        if (ObjectUtils.isEmpty(array)) {
+            log.error(printLog);
+            throw new BusinessException(message);
+        }
+    }
+
     public static void notEmpty(Collection<?> collection, IBaseErrorCode message) {
         if (CollectionUtils.isEmpty(collection)) {
             throw new BusinessException(message);
         }
     }
 
-    /**
-     * Assert that a Map contains entries; that is, it must not be {@code null}
-     * and must contain at least one entry.
-     * <pre class="code">Assert.notEmpty(map, "Map must contain entries");</pre>
-     *
-     * @param map     the map to check
-     * @param message the exception message to use if the assertion fails
-     * @throws IllegalArgumentException if the map is {@code null} or contains no entries
-     */
+    public static void notEmpty(Collection<?> collection, IBaseErrorCode message, String printLog) {
+        if (CollectionUtils.isEmpty(collection)) {
+            log.error(printLog);
+            throw new BusinessException(message);
+        }
+    }
+
     public static void notEmpty(Map<?, ?> map, IBaseErrorCode message) {
         if (CollectionUtils.isEmpty(map)) {
             throw new BusinessException(message);
         }
     }
 
-    /**
-     * Assert that an array contains no {@code null} elements.
-     * <p>Note: Does not complain if the array is empty!
-     * <pre class="code">Assert.noNullElements(array, "The array must contain non-null elements");</pre>
-     *
-     * @param array   the array to check
-     * @param message the exception message to use if the assertion fails
-     * @throws IllegalArgumentException if the object array contains a {@code null} element
-     */
+    public static void notEmpty(Map<?, ?> map, IBaseErrorCode message, String printLog) {
+        if (CollectionUtils.isEmpty(map)) {
+            log.error(printLog);
+            throw new BusinessException(message);
+        }
+    }
+
     public static void noNullElements(Object[] array, IBaseErrorCode message) {
         if (array != null) {
             for (Object element : array) {
