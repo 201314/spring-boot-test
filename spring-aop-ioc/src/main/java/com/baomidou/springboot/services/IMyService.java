35915d8cb49a4c6c85f4762f9519f756f4384943
linzl <2225010489@qq.com>
2021-11-25 17:20:15 +0800

    * 切面整理

--- spring-aop-ioc/src/main/java/com/baomidou/springboot/services/IMyService.java
+++ spring-aop-ioc/src/main/java/com/baomidou/springboot/services/IMyService.java
@@ -1,9 +1,18 @@
 package com.baomidou.springboot.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.gitee.linzl.commons.api.ApiResult;
 
 public interface IMyService {
     default public void add() {
+    }
+
+    default public void select1() {
+
+    }
+
+    default public void select2(){
+
     }
 
     default public void doSomeThing(String someThing) {
--- spring-aop-ioc/src/main/java/com/baomidou/springboot/services/MyService.java
+++ spring-aop-ioc/src/main/java/com/baomidou/springboot/services/MyService.java
@@ -1,19 +1,75 @@
 package com.baomidou.springboot.services;
 
+import com.baomidou.springboot.Test;
+import com.baomidou.springboot.entity.Product;
+import com.baomidou.springboot.entity.TpTradeOrder;
+import com.baomidou.springboot.mapper.ProductMapper;
+import com.baomidou.springboot.mapper.TradeOrderMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gitee.linzl.commons.annotation.RpcExceptionCatch;
 import com.gitee.linzl.commons.api.ApiResult;
 import com.gitee.linzl.commons.enums.BaseErrorCode;
 import com.gitee.linzl.commons.exception.BusinessException;
 import com.gitee.linzl.commons.tools.ApiResults;
+import com.gitee.linzl.datasource.DynamicDataSourceHolder;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+
 public class MyService implements IMyService {
+    @Autowired
+    private ProductMapper productMapper;
+
+    @Autowired
+    private TradeOrderMapper tradeOrderMapper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Override
+    @Test
     public void add() {
-        log.debug("========MyService add========");
+        Product product = new Product();
+        product.setName("我是写库：" + RandomStringUtils.randomAlphabetic(10));
+        productMapper.insertSelective(product);
+    }
+
+    @Override
+    @Test
+    public void select1() {
+        Long id = RandomUtils.nextLong();
+        TpTradeOrder order = new TpTradeOrder();
+        order.setId(id);
+        // 1 待支付，2支付中，3支付成功，4支付失败，5支付关闭
+        order.setTradeStatus("1");
+        order.setHello("select1:随意1");
+        order.setCreatedBy("创建者" + RandomStringUtils.randomNumeric(10));
+        order.setUpdatedBy("修改者1");
+        tradeOrderMapper.insertSelective(order);
+
+        TpTradeOrder order2 = tradeOrderMapper.selectById(id);
+    }
+
+    @Override
+    @Test
+    public void select2() {
+        Long id = RandomUtils.nextLong();
+        TpTradeOrder order = new TpTradeOrder();
+        order.setId(id);
+        // 1 待支付，2支付中，3支付成功，4支付失败，5支付关闭
+        order.setTradeStatus("1");
+        order.setHello("select2:随意1");
+        order.setCreatedBy("创建者" + RandomStringUtils.randomNumeric(10));
+        order.setUpdatedBy("修改者1");
+        tradeOrderMapper.insertSelective(order);
+
+        TpTradeOrder order2 = tradeOrderMapper.selectById(123456L);
     }
 
     @Override
