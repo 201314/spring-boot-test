9c2b4f42fab2e018fb5d1839bf883fa574188a6a
linzl <2225010489@qq.com>
2022-02-16 11:24:19 +0800

    * fastjson 配置ASM

--- spring-commons/src/main/java/com/gitee/linzl/commons/config/json/JsonConfiguration.java
+++ spring-commons/src/main/java/com/gitee/linzl/commons/config/json/JsonConfiguration.java
@@ -7,12 +7,13 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
-
+import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.serializer.JSONSerializer;
 import com.alibaba.fastjson.serializer.ObjectSerializer;
 import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializeWriter;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
@@ -52,14 +53,16 @@ public class JsonConfiguration implements WebMvcConfigurer {
         }
 
         if (fastJsonPresent) {
-            FastJsonHttpMessageConverter jsonConverter = new FastJsonHttpMessageConverter();
-            com.alibaba.fastjson.support.config.FastJsonConfig fastJsonConfig = new com.alibaba.fastjson.support.config.FastJsonConfig();
-
             SerializeConfig serializeConfig = SerializeConfig.getGlobalInstance();
+            // 序列化: 安卓环境下默认关闭
+            serializeConfig.setAsmEnable(Boolean.TRUE);
+
             // 支持 LocalDateTime 、LocalDate 转换成long
             LocalDateToLongSerializer instance = new LocalDateToLongSerializer();
             serializeConfig.put(LocalDateTime.class, instance);
             serializeConfig.put(LocalDate.class, instance);
+
+            FastJsonConfig fastJsonConfig = new FastJsonConfig();
             // 驼峰转下划线
             // FastJson配置驼峰命名规则会导致spring boot admin 或者 swagger-ui无法正常使用
             fastJsonConfig.setSerializeConfig(serializeConfig);
@@ -72,6 +75,8 @@ public class JsonConfiguration implements WebMvcConfigurer {
             // WriteNullBooleanAsFalse 将Boolean类型字段的空值输出为false
             fastJsonConfig.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect,
                 SerializerFeature.WriteMapNullValue);
+
+            FastJsonHttpMessageConverter jsonConverter = new FastJsonHttpMessageConverter();
             jsonConverter.setFastJsonConfig(fastJsonConfig);
 
             // 解决中文乱码问题
@@ -80,6 +85,9 @@ public class JsonConfiguration implements WebMvcConfigurer {
             jsonConverter.setSupportedMediaTypes(mediaTypes);
 
             converters.add(0, jsonConverter);
+
+            // 反序列化: 安卓环境下默认关闭
+            ParserConfig.getGlobalInstance().setAsmEnable(Boolean.TRUE);
             if (log.isDebugEnabled()) {
                 log.debug("初始化fastjson,默认时间格式yyyy-MM-dd HH:mm:ss");
             }
