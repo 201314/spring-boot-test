950233bb780cdb6d69d81d7ef7f7c9adc5ee1a54
linzl <2225010489@qq.com>
2022-04-08 10:17:37 +0800

    * socket

--- spring-websocket/src/main/java/com/gitee/linzl/controller/DemoController.java
+++ spring-websocket/src/main/java/com/gitee/linzl/controller/DemoController.java
@@ -1,31 +1,26 @@
 package com.gitee.linzl.controller;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
+import com.gitee.linzl.service.WebSocketServer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gitee.linzl.service.WebSocketServer;
-
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/demo")
 @Slf4j
 public class DemoController {
-	@GetMapping(value = "/pushVideoListToWeb")
-	public Map<String, Object> pushVideoListToWeb() {
-		log.debug("socket 主动发布");
-		Map<String, Object> result = new HashMap<String, Object>();
-		try {
-			WebSocketServer.sendInfo("有新客户呼入,sltAccountId" + Math.random());
-			result.put("operationResult", true);
-		} catch (IOException e) {
-			result.put("operationResult", true);
-		}
-		return result;
-	}
+    @GetMapping(value = "/push/{userId}")
+    public Map<String, Object> push(@PathVariable("userId") String userId) {
+        log.debug("socket 主动发布");
+        Map<String, Object> result = new HashMap<String, Object>();
+        WebSocketServer.broadcast("有新客户呼入,sltAccountId" + Math.random(), userId);
+        result.put("operationResult", true);
+        return result;
+    }
 }
